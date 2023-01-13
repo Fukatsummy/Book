b5760e60468dc49824f9aefdc9bb8bf639936eba
student <fukatsummy@gmail.com>
2023-01-13 19:07:36 +0300

    Add project files.

---
+++ Book/SQLClass.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SQLConnect
+{
+    internal class SQLClass
+    {
+        SqlConnection connection = null;
+        public SQLClass()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["localdbConnection"].ConnectionString;
+            /*string connectionString = @"Data Source = \\127.134.052.017\\SQLEXPRESS
+(localdb)\MSSQLLocalDB; Initial Catalog = Library;Integrated Security = False;User= Password=";*/
+            connection = new SqlConnection(connectionString);
+        }
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+        public void InsertCommand(string commandString)
+        {
+            SqlCommand command = new SqlCommand(commandString, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
